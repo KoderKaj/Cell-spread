161a8e85eba416d546138ff35d59fce57de0e8f1
Kajuran <znac167@live.rhul.ac.uk>
2026-02-26 22:18:35 +0000

    Added a simple button and a function to generate a grid. Currently the grid is only a checkerboard pattern when the size is odd, but that will be fixed in the next commit.

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 // JavaScript source code
+function makeGrid(size) {
+    const grid = document.getElementById("grid");
+    grid.innerHTML = '';
+
+    grid.style.gridTemplateColumns = `repeat(${size}, 100px)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 100px)`;
+
+    for (let i = 0; i < size * size; i++) {
+        const tile = document.createElement('div');
+        tile.classList.add('tile');
+        grid.appendChild(tile);
+    }
+}
+
+//makeGrid(10);
